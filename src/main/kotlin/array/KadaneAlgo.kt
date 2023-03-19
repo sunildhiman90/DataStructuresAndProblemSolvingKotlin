7ffc5146d3b9b4df5012b16293e6278959a6585b
Sunil Kumar <sunil.dhiman90@gmail.com>
2023-03-20 00:03:46 +0530

    Kadane algo

---
+++ src/main/kotlin/array/KadaneAlgo.kt
@@ -0,0 +1,25 @@
+package array
+
+class KadaneAlgo {
+}
+
+fun kadaneAlgo(arr: List<Int>): Int {
+    var sumGlobal = Int.MIN_VALUE
+    var sumSoFar = 0
+
+    for (i in arr) {
+        sumSoFar += i
+        if(sumSoFar > sumGlobal) {
+            sumGlobal = sumSoFar
+        }
+        if (sumSoFar < 0) {
+            sumSoFar = 0 //reset
+        }
+    }
+    return sumGlobal;
+}
+
+fun main() {
+    val arr = intArrayOf(1, 1, -1, -2, 6, 1, -2, 3)
+    println(kadaneAlgo(arr.toList()))
+}
