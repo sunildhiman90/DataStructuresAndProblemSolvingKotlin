07613aa3f8f49611a780beefe148849ef20ecc47
Sunil Kumar <sunil.dhiman90@gmail.com>
2023-07-19 14:19:58 +0530

    NextSmallerElement and NextGreaterElement modified

--- src/main/kotlin/problemsolving/monotonicstack/NextGreaterElement.kt
+++ src/main/kotlin/problemsolving/monotonicstack/NextGreaterElement.kt
@@ -9,11 +9,50 @@ class NextGreaterElement {
 
 fun main() {
 
-    val arr = arrayOf(11,13,21,3)
+    val arr = arrayOf(11, 13, 21, 3, 5, 3)
+    println(nextGreaterElementMain(arr).contentToString())
     println(nextGreaterElement(arr).contentToString())
 
 }
 
+fun nextGreaterElementMain(nums: Array<Int>): Array<Int> {
+    val finalArray = Array(nums.size) { 0 }
+    val stack = Stack<Int>()
+    var next: Int
+
+    for ((index, element) in nums.withIndex()) {
+
+        // next num
+        next = element
+
+        //use decreasing monotonic stack logic for popping, becoz if current(next) element is smaller than prev(stack top),
+        // then it does not satisfy our criteria, so push to stack for later adding -1 so stack will contain elements which does not satisfy our criteria ,
+        // otherwise pop from stack becoz our criteria is satisfied
+        while (!stack.isEmpty() && nums[stack.peek()] < next) {
+            finalArray[stack.peek()] = next
+            stack.pop()
+        }
+
+        //push index for finding its next greater, becoz we need index for creating new array
+        stack.push(index)
+
+    }
+
+    //print -1 for all remaining
+    while (!stack.isEmpty()) {
+        finalArray[stack.peek()] = -1
+        stack.pop()
+    }
+
+    return finalArray
+}
+
+
+//this apparoach wil not work with repeating elements at non consecutive(not continuously but with some other elements in between) indexes , then for last index repeating element,
+// if no greater element found, then it will replace greater element of prev repeating element
+// For example check this case: 11, 13, 21, 3, 5, 3
+// Here 3 is repeated at not consecutive indexes(3rd and 5th indexes), So for first 3 at 3rd index, it will put next greater as 5,
+// but for last 3 at 5th index in 2nd while loop it will replace it with -1 becoz after that we dont have greater element, Becoz hashmap cannot have multiple same keys with different values
 fun nextGreaterElement(nums: Array<Int>): Array<Int> {
     val hashMap = mutableMapOf<Int, Int>()
     val stack = Stack<Int>()
@@ -22,21 +61,21 @@ fun nextGreaterElement(nums: Array<Int>): Array<Int> {
 
     stack.push(element)
 
-    for(i in 1 until nums.size) {
+    for (i in 1 until nums.size) {
 
         // next num
         next = nums[i]
 
-        if(!stack.isEmpty()) {
+        if (!stack.isEmpty()) {
             //pop element for checking
             element = stack.pop()
 
-            while(element < next) {
+            while (element < next) {
                 hashMap[element] = next
 
                 // We are not using this stack is not empty condition in while loop: while(!stack.isEmpty() && element < next)
                 // becoz we are already popping one element before loop, so then this logic will not work for single element in stack
-                if(stack.isEmpty()) break
+                if (stack.isEmpty()) break
                 //pop and check for popped element next time
                 element = stack.pop()
             }
@@ -53,7 +92,7 @@ fun nextGreaterElement(nums: Array<Int>): Array<Int> {
     }
 
     //print -1 for all remaining
-    while(!stack.isEmpty()) {
+    while (!stack.isEmpty()) {
         element = stack.pop()
         hashMap[element] = -1
     }
@@ -61,8 +100,8 @@ fun nextGreaterElement(nums: Array<Int>): Array<Int> {
     val finalArr = Array(nums.size) {
         0
     }
-    for((index, num) in nums.withIndex()) {
-        if(hashMap.contains(num)) {
+    for ((index, num) in nums.withIndex()) {
+        if (hashMap.contains(num)) {
             hashMap.get(num)?.let {
                 finalArr[index] = it
             }
--- src/main/kotlin/problemsolving/monotonicstack/NextSmallerElement.kt
+++ src/main/kotlin/problemsolving/monotonicstack/NextSmallerElement.kt
@@ -3,67 +3,88 @@ package problemsolving.monotonicstack
 import java.util.*
 
 /**
- Find next smaller element of each element of array after that element
+Find next smaller element of each element of array after that element
  */
- // Related Problem links :
- // https://practice.geeksforgeeks.org/problems/fab3dbbdce746976ba35c7b9b24afde40eae5a04/1
+// Related Problem links :
+// https://practice.geeksforgeeks.org/problems/fab3dbbdce746976ba35c7b9b24afde40eae5a04/1
 
 fun main() {
 
-    val arr = arrayOf(11,13,4,21,3)
-    println(nextSmallerElement(arr).contentToString())
+    //val arr = arrayOf(11,13,4,21,3)
+    //val arr = arrayOf(10,1,1,6)
+    val arr = arrayOf(10, 1, 1, 6, 5, 3, 7, 9, 1, 3, 4)
+    println(nextSmallerElement2(arr).contentToString())
+    println(nextSmallerElementMain(arr).contentToString())
 
 }
 
-fun nextSmallerElement(nums: Array<Int>): Array<Int> {
-    val hashMap = mutableMapOf<Int, Int>()
+fun nextSmallerElementMain(nums: Array<Int>): Array<Int> {
+    val finalArray = Array(nums.size) { 0 }
     val stack = Stack<Int>()
-    var element = nums[0]
     var next: Int
 
-    stack.push(element)
-
-    for(i in 1 until nums.size) {
+    for ((index, element) in nums.withIndex()) {
 
         // next num
-        next = nums[i]
+        next = element
 
-        if(!stack.isEmpty()) {
-            //pop element for checking
-            element = stack.pop()
-
-            while(element > next) {
-                hashMap[element] = next
-
-                // We are not using this stack is not empty condition in while loop: while(!stack.isEmpty() && element < next)
-                // becoz we are already popping one element before loop, so then this logic will not work for single element in stack
-                if(stack.isEmpty()) break
-                //pop and check for popped element next time
-                element = stack.pop()
-            }
-
-            //element is smaller than next, then push it back, It wil make sure elements in stack will be in increasing order
-            if (element < next)
-                stack.push(element)
-
+        //use increasing monotonic stack logic for popping, becoz if current(next) element is larger than prev(stack top),
+        // then it does not satisfy our criteria, so push to stack for later adding -1, otherwise pop
+        while (!stack.isEmpty() && nums[stack.peek()] > next) {
+            finalArray[stack.peek()] = next
+            stack.pop()
         }
 
-        //push next for finding its next greater
+        //push index for finding its next smaller, becoz we need index for creating new array
+        stack.push(index)
+
+    }
+
+    //print -1 for all remaining
+    while (!stack.isEmpty()) {
+        finalArray[stack.peek()] = -1
+        stack.pop()
+    }
+
+    return finalArray
+}
+
+//this apparoach wil not work with repeating elements, then for last index repeating element,
+// if no smaller element found, then it will replace smaller element of prev repeating element
+// For example check this case: 10, 1, 1, 6, 5, 3, 7, 9, 1, 3, 4
+// Here 3 is repeated at not consecutive indexes, So for first 3, it will put next smaller as 1,
+// but for last 3 it will replace it with -1 becoz after that we dont have smaller element
+fun nextSmallerElement2(nums: Array<Int>): Array<Int> {
+    val hashMap = mutableMapOf<Int, Int>()
+    val stack = Stack<Int>()
+    var next: Int
+
+    for (element in nums) {
+
+        // next num
+        next = element
+
+        //use increasing monotonic stack logic for popping, becoz if current(next) element is larger than prev(stack top),
+        // then it does not satisfy our criteria, so push to stack for later adding -1, otherwise pop
+        while (!stack.isEmpty() && stack.peek() > next) {
+            hashMap[stack.pop()] = next
+        }
+
+        //push next for finding its next smaller
         stack.push(next)
 
     }
 
     //print -1 for all remaining
-    while(!stack.isEmpty()) {
-        element = stack.pop()
-        hashMap[element] = -1
+    while (!stack.isEmpty()) {
+        hashMap[stack.pop()] = -1
     }
 
     val finalArr = Array(nums.size) {
         0
     }
-    for((index, num) in nums.withIndex()) {
-        if(hashMap.contains(num)) {
+    for ((index, num) in nums.withIndex()) {
+        if (hashMap.contains(num)) {
             hashMap.get(num)?.let {
                 finalArr[index] = it
             }
