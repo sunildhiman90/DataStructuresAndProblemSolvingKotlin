e07bb99ffddd0da092729c731c10d9b89c6a12b2
Sunil Kumar <sunil.dhiman90@gmail.com>
2023-07-20 10:30:18 +0530

    NextSmallerElement2 and NextGreaterElement modified

--- src/main/kotlin/problemsolving/monotonicstack/NextGreaterElement.kt
+++ src/main/kotlin/problemsolving/monotonicstack/NextGreaterElement.kt
@@ -10,10 +10,43 @@ class NextGreaterElement {
 fun main() {
 
     val arr = arrayOf(11, 13, 21, 3, 5, 3)
+    println(nextGreaterElementMainCompacted(arr).contentToString())
     println(nextGreaterElementMain(arr).contentToString())
-    println(nextGreaterElement(arr).contentToString())
+    println(nextGreaterElementUsingHashmap(arr).contentToString())
 
 }
+
+
+// compacted code as compared to other solutions here
+fun nextGreaterElementMainCompacted(nums: Array<Int>): Array<Int> {
+    // directly initialize array to -1 here for all elements, and later for which we will find greater only that will be updated
+    // and we dont need 2nd while loop for putting -1 for remaining
+    val finalArray = Array(nums.size) { -1 }
+    val stack = Stack<Int>()
+    var next: Int
+
+    for ((index, element) in nums.withIndex()) {
+
+        // next num
+        next = element
+
+        // Find all elements for which next is greater..
+        // thats why use decreasing monotonic stack logic for popping, becoz if current(next) element is smaller than prev(stack top),
+        // then it does not satisfy our criteria, so push to stack for later adding -1 so stack will contain elements which does not satisfy our criteria ,
+        // otherwise pop from stack becoz our criteria is satisfied
+        while (!stack.isEmpty() && nums[stack.peek()] < next) {
+            finalArray[stack.peek()] = next
+            stack.pop()
+        }
+
+        //push index for finding its next greater, becoz we need index for creating new array
+        stack.push(index)
+
+    }
+
+    return finalArray
+}
+
 
 fun nextGreaterElementMain(nums: Array<Int>): Array<Int> {
     val finalArray = Array(nums.size) { 0 }
@@ -25,7 +58,8 @@ fun nextGreaterElementMain(nums: Array<Int>): Array<Int> {
         // next num
         next = element
 
-        //use decreasing monotonic stack logic for popping, becoz if current(next) element is smaller than prev(stack top),
+        // Find all elements for which next is greater..
+        // thats why use decreasing monotonic stack logic for popping, becoz if current(next) element is smaller than prev(stack top),
         // then it does not satisfy our criteria, so push to stack for later adding -1 so stack will contain elements which does not satisfy our criteria ,
         // otherwise pop from stack becoz our criteria is satisfied
         while (!stack.isEmpty() && nums[stack.peek()] < next) {
@@ -48,53 +82,35 @@ fun nextGreaterElementMain(nums: Array<Int>): Array<Int> {
 }
 
 
-//this apparoach wil not work with repeating elements at non consecutive(not continuously but with some other elements in between) indexes , then for last index repeating element,
+// this approach wil not work with repeating elements at non consecutive(not continuously but with some other elements in between) indexes , then for last index repeating element,
 // if no greater element found, then it will replace greater element of prev repeating element
 // For example check this case: 11, 13, 21, 3, 5, 3
 // Here 3 is repeated at not consecutive indexes(3rd and 5th indexes), So for first 3 at 3rd index, it will put next greater as 5,
 // but for last 3 at 5th index in 2nd while loop it will replace it with -1 becoz after that we dont have greater element, Becoz hashmap cannot have multiple same keys with different values
-fun nextGreaterElement(nums: Array<Int>): Array<Int> {
+fun nextGreaterElementUsingHashmap(nums: Array<Int>): Array<Int> {
     val hashMap = mutableMapOf<Int, Int>()
     val stack = Stack<Int>()
-    var element = nums[0]
-    var next = -1
+    var next: Int
 
-    stack.push(element)
-
-    for (i in 1 until nums.size) {
+    for (element in nums) {
 
         // next num
-        next = nums[i]
+        next = element
 
-        if (!stack.isEmpty()) {
-            //pop element for checking
-            element = stack.pop()
-
-            while (element < next) {
-                hashMap[element] = next
-
-                // We are not using this stack is not empty condition in while loop: while(!stack.isEmpty() && element < next)
-                // becoz we are already popping one element before loop, so then this logic will not work for single element in stack
-                if (stack.isEmpty()) break
-                //pop and check for popped element next time
-                element = stack.pop()
-            }
-
-            //element is greater than next, then push it back, It wil make sure elements in stack will be in decreasing order
-            if (element > next)
-                stack.push(element)
-
+        //use decreasing monotonic stack logic for popping, becoz if current(next) element is smaller than prev(stack top),
+        // then it does not satisfy our criteria, so push to stack for later adding -1, otherwise pop
+        while (!stack.isEmpty() && stack.peek() < next) {
+            hashMap[stack.pop()] = next
         }
 
-        //push next for finding its next greater
+        //push next for finding its next smaller
         stack.push(next)
 
     }
 
     //print -1 for all remaining
     while (!stack.isEmpty()) {
-        element = stack.pop()
-        hashMap[element] = -1
+        hashMap[stack.pop()] = -1
     }
 
     val finalArr = Array(nums.size) {
---
+++ src/main/kotlin/problemsolving/monotonicstack/NextGreaterElement2.kt
@@ -0,0 +1,87 @@
+package problemsolving.monotonicstack
+
+import java.util.*
+
+// Find next greater element of each element of array after that element, we can search in circular array
+// https://leetcode.com/problems/next-greater-element-ii/
+
+fun main() {
+
+    val arr = arrayOf(1,2,3,4,3)
+    println(nextGreaterElement2MainCompact(arr).contentToString())
+    println(nextGreaterElement2Main(arr).contentToString())
+
+}
+
+// More compacted code as compared to solution 2 given below
+fun nextGreaterElement2MainCompact(nums: Array<Int>): IntArray {
+    //directly initialize array to -1 here for all elements, and later for which we will find greater only that will be updated
+    // and we dont need 2nd while loop for putting -1 for remaining
+    val finalArray = IntArray(nums.size) { -1 }
+    val stack = Stack<Int>()
+    var next: Int
+
+    //loop twice for making circular array
+    for (i in 0 until 2 * nums.size) {
+
+        val circularIndex = i % nums.size
+
+        // next num
+        next = nums[circularIndex]
+
+        // use decreasing monotonic stack logic for popping, becoz if current(next) element is smaller than prev(stack top),
+        // then it does not satisfy our criteria, so push to stack for later adding -1
+        // so stack will contain elements which does not satisfy our criteria ,
+        // otherwise pop from stack becoz our criteria is satisfied
+        while (!stack.isEmpty() && nums[stack.peek()] < next) {
+            finalArray[stack.peek()] = next
+            stack.pop()
+        }
+
+        // becoz we are looping in circular array, same index will come twice,
+        // so push circularIndex only if its not found yet(if finalArray[circularIndex] == -1) for finding its next greater
+        // this index will be used for creating new array,
+        if(finalArray[circularIndex] == -1)
+            stack.push(circularIndex)
+
+    }
+
+    return finalArray
+}
+
+
+// Solution 2
+fun nextGreaterElement2Main(nums: Array<Int>): Array<Int> {
+    val finalArray = Array(nums.size) { 0 }
+    val stack = Stack<Int>()
+    var next: Int
+
+    //loop twice for making circular array
+    for (i in 0 until 2 * nums.size) {
+
+        val circularIndex = i % nums.size
+        // next num
+        next = nums[circularIndex]
+
+        //use decreasing monotonic stack logic for popping, becoz if current(next) element is smaller than prev(stack top),
+        // then it does not satisfy our criteria, so push to stack for later adding -1 so stack will contain elements which does not satisfy our criteria ,
+        // otherwise pop from stack becoz our criteria is satisfied
+        while (!stack.isEmpty() && nums[stack.peek()] < next) {
+            finalArray[stack.peek()] = next
+            stack.pop()
+        }
+
+        //push circularIndex only if its not found yet(if finalArray[circularIndex] == 0) for finding its next greater, becoz we need index for creating new array, use circular condition for index so that index will be correct only from 0 to size-1
+        if(finalArray[circularIndex] == 0)
+            stack.push(circularIndex)
+
+    }
+
+    //print -1 for all remaining
+    while (!stack.isEmpty()) {
+        finalArray[stack.peek()] = -1
+        stack.pop()
+    }
+
+    return finalArray
+}
