b75396c92d246559be40588852685df8dc2cb117
Sunil Kumar <sunil.dhiman90@gmail.com>
2023-07-21 11:05:15 +0530

    Added logic for disconnected Graph in GraphBFS

--- src/main/kotlin/datastructures/graph/GraphBFS.kt
+++ src/main/kotlin/datastructures/graph/GraphBFS.kt
@@ -14,34 +14,92 @@ class GraphBFS(
         adjacencyList[to].add(from)
     }
 
-    //TODO, calculate level using BFS, BFS Traversal is also called level traversal of graph
-    //TODO, handle disconnected graph case as well, in that case all nodes will not be traversed using current algo
-
-
     fun bfsTraversal(startVertex: Int) {
-        val visited: Array<Boolean> = Array(numberOfVertices) { false } //instead of Boolean array, we can use Set as well
+        val visited: Array<Boolean> =
+            Array(numberOfVertices) { false } //instead of Boolean array, we can use Set as well
+        bfsHelper(startVertex, visited)
+
+        // for disconnected graph
+        // check if still some vertex is still unvisited
+        for (i in visited.indices) {
+            if (!visited[i]) {
+                bfsHelper(i, visited)
+            }
+        }
+    }
+
+    private fun bfsHelper(startVertex: Int, visited: Array<Boolean>) {
         val queue: LinkedList<Int> = LinkedList()
 
+        // mark visited and add to queue for printing and then checking its neighbors
         visited[startVertex] = true
         queue.add(startVertex)
 
         while (!queue.isEmpty()) {
+
+            //poll and print
             val currentVertex = queue.poll()
             println("$currentVertex ")
 
+            //check for its neighbours
             for (element in adjacencyList[currentVertex]) {
-                if(!visited[element]) {
+                if (!visited[element]) {
                     visited[element] = true
                     queue.add(element)
                 }
             }
         }
+
+    }
+
+    fun bfsTraversalWithLevel(startVertex: Int) {
+        val visited: Array<Boolean> =
+            Array(numberOfVertices) { false } //instead of Boolean array, we can use Set as well
+        val levels: Array<Int> = Array(numberOfVertices) { 0 }
+
+        bfsHelperWithLevel(startVertex, visited, levels)
+
+        // for disconnected graph
+        // check if still some vertex is still unvisited
+        for (i in visited.indices) {
+            if (!visited[i]) {
+                bfsHelperWithLevel(i, visited, levels)
+            }
+        }
+    }
+
+    private fun bfsHelperWithLevel(startVertex: Int, visited: Array<Boolean>, levels: Array<Int>) {
+        val queue: LinkedList<Int> = LinkedList()
+
+        // mark visited and add to queue for printing and then checking its neighbors
+        visited[startVertex] = true
+        levels[startVertex] = 0
+        queue.add(startVertex)
+
+        while (!queue.isEmpty()) {
+
+            //poll and print
+            val currentVertex = queue.poll()
+            println("$currentVertex --- level from parent: ${levels[currentVertex]}")
+
+            //check for its neighbours
+            for (element in adjacencyList[currentVertex]) {
+                if (!visited[element]) {
+                    visited[element] = true
+                    levels[element] = levels[currentVertex] + 1 //get level of parent and increase it
+                    queue.add(element)
+                }
+            }
+        }
+
     }
 }
 
 fun main() {
-    val graph = GraphBFS(6)
 
+    println("---Connected graph--- ")
+
+    val graph = GraphBFS(6)
     graph.addEdge(0, 1)
     graph.addEdge(0, 2)
     graph.addEdge(0, 5)
@@ -49,7 +107,33 @@ fun main() {
     graph.addEdge(2, 4)
 
     graph.bfsTraversal(0) //0,1,2,5,3,4
+    graph.bfsTraversalWithLevel(0) //0,1,2,5,3,4
+
+
+    println("---Disconnected graph--- ")
+
+    val disconnectedGraph = GraphBFS(8)
+    disconnectedGraph.addEdge(0, 1)
+    disconnectedGraph.addEdge(0, 2)
+    disconnectedGraph.addEdge(0, 5)
+    disconnectedGraph.addEdge(1, 3)
+    disconnectedGraph.addEdge(2, 4)
+    disconnectedGraph.addEdge(6, 7)
+    disconnectedGraph.bfsTraversal(0) //0,1,2,5,3,4,6,7
 }
+
+/**
+ * Theory
+ * Breadth-First Search focuses on traversing Breadth wise, i.e.
+ * we will first traverse all the neighbours of A first, and then explore their neighbours, and so on.
+ *
+ * Level: The level can be thought of as the distance from the starting Node, like 1, 2 and 5 are at distance 1 from 0
+ * so they have level 1, and similarly, 3 is at distance 2(as it connects to 0 via 1 in between) wrt 0.
+ *
+ * So as we can see in the above image, we traverse the elements of each level first, before traversing the elements of other levels.
+ * Due to the same reason, Breadth First Search can be also thought of as a Level Order Traversal
+ */
+
 
 /**
  * Complexity Analysis of BFS
@@ -66,6 +150,30 @@ We use two data structures in Breadth-First Search - the visited array/Set, and 
 A Visited array will have the size of the number of vertices in the graph (Similarly, the maximum size of a Set can also be up to the number of vertices in the graph).
 
 Queue carries the vertices, as it explores them, and the maximum nodes it can have is again as many as the vertices in the Graph. E.g. If the starting vertex is 0 and all other vertices are connected to 0, then when we explore 0, we add all other vertices(as those are neighbours of 0) to Queue. So, the Queue will carry V-1 vertices in the worst case.
-
 Since both the Visited array and Queue can have a max size of V(equal to as many vertices), the overall space complexity will be O(V).
+
+It can also be noted here that the levels dictate the shortest distance(i.e. Number of edges) between two Nodes. E.g. if we start from A, then D is seen at level 1, so A and D have a shortest distance of 1 between them. Similarly, as B is seen at level 2, A and B have a shortest distance of 2.
+
+So in case if you are ever interested in finding the shortest distance between any two nodes of a graph, Breadth-First Search is your best bet.
+
+You just need to perform a Breadth First Search from one of the two nodes and find the level of the other node. This level will be your shortest distance between those two nodes.
+ */
+
+/**
+ * Applications of Breadth First Search Algorithm
+ *
+ * Let’s try to see where does Breadth-First Search finds its applications:
+ *
+ * 1. Minimum spanning tree for unweighted graphs- In Breadth-First Search we can reach from any given source vertex to another vertex,
+ * with the minimum number of edges, and this principle can be used to find the minimum spanning tree which is the path covering all vertices in the shortest paths.
+ * 2. Peer to peer networking: In Peer to peer networking, to find the neighbouring peer from any other peer, the Breadth-First Search is used.
+ * 3. Crawlers in search engines: Search engines need to crawl the internet. To do so, they can start from any source page,
+ * follow the links contained in that page in the Breadth-First Search manner, and therefore explore other pages.
+ * 4. GPS navigation systems: To find locations within a given radius from any source person, we can find all neighbouring locations
+ * using the Breadth-First Search, and keep on exploring until those are within the K radius.
+ * 5. Broadcasting in networks: While broadcasting from any source, we find all its neighbouring peers and continue broadcasting to them, and so on.
+ * 6. Path Finding: To find if there is a path between 2 vertices, we can take any vertex as a source, and keep on traversing until we reach the destination vertex.
+ * If we explore all vertices reachable from the source and could not find the destination vertex, then that means there is no path between these 2 vertices.
+ * 7. Finding all reachable Nodes from a given Vertex: All vertices which are reachable from a given vertex can be found using the BFS approach in any disconnected graph.
+ * The vertices which are marked as visited in the visited array after the BFS is complete contains all those reachable vertices.
  */
--- src/main/kotlin/datastructures/graph/GraphDFS.kt
+++ src/main/kotlin/datastructures/graph/GraphDFS.kt
@@ -65,12 +65,12 @@ fun main() {
     disconnectedGraph.addEdge(0, 5)
     disconnectedGraph.addEdge(1, 3)
     disconnectedGraph.addEdge(2, 4)
-    disconnectedGraph.addEdge(6, 7)
-    disconnectedGraph.dfsTraversal2(0) //0,1,3,2,4,5
+    disconnectedGraph.addEdge(6, 7) //disconnected part
+    disconnectedGraph.dfsTraversal2(0) //0,1,3,2,4,5,6,7
 }
 
 /**
- * Theory:
+ * Theory In Short:
  * The idea of the depth-first search algorithm is to start from an arbitrary node of the graph
  * and to explore as far as possible before coming back i.e. moving to an adjacent node until there is no unvisited adjacent node.
  * Then backtrack and check for other unvisited nodes and repeat the same process for them.
@@ -80,12 +80,41 @@ fun main() {
 
 /**
  * Complexity Analysis of DFS Algorithm
-Time complexity: Since we consider every vertex exactly once and every edge atmost twice(once while backtracking), therefore time complexity is
-O(V+2E)≃O(V+E), where
-V is the number of vertices and
-E is the number of edges in the given graph.
+ * Time complexity: Since we consider every vertex exactly once and every edge atmost twice(once while backtracking), therefore time complexity is
+ * O(V+2E)≃O(V+E), where
+ * V is the number of vertices and
+ * E is the number of edges in the given graph.
 
-Space Complexity:
-O(V), because we need an auxiliary visited array/set of size V
+ * Space Complexity:
+ * O(V), because we need an auxiliary visited array/set of size V
+ */
+
+/**
  *
- * */
+ * Applications of Depth First Search:
+ *
+ * 1. Detecting cycle in a graph: A graph has a cycle if and only if we see a back edge during DFS.
+ * So we can run DFS for the graph and check for back edges.
+ *
+ * 2. Path Finding: We can specialize the DFS algorithm to find a path between two given vertices u and z.
+ *
+ * Call DFS(G, u) with u as the start vertex.
+ * Use a stack S to keep track of the path between the start vertex and the current vertex.
+ * As soon as destination vertex z is encountered, return the path as the contents of the stack
+ *
+ * 3. Topological Sorting: Topological Sorting is mainly used for scheduling jobs from the given dependencies among jobs.
+ * In computer science, applications of this type arise in instruction scheduling, ordering of formula cell evaluation
+ * when recomputing formula values in spreadsheets, logic synthesis, determining the order of compilation tasks to perform in makefiles,
+ * data serialization, and resolving symbol dependencies in linkers.
+ *
+ * 4. To test if a graph is bipartite: We can augment either BFS or DFS when we first discover a new vertex,
+ * color it opposite its parents, and for each other edge, check it doesn’t link two vertices of the same color.
+ * The first vertex in any connected component can be red or black.
+ *
+ * 5. Finding Strongly Connected Components of a graph: A directed graph is called strongly connected if
+ * there is a path from each vertex in the graph to every other vertex.
+ *
+ * 6. Solving puzzles with only one solution: such as mazes.
+ * (DFS can be adapted to find all solutions to a maze by only including nodes on the current path in the visited set.)
+ *
+ */
